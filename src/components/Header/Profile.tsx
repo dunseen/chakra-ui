3c2ecf9e5f4509c9c27e2d7ca28dde98474d45ae
Davys Lima <davysjunior08@hotmail.com>
2021-08-03 23:33:44 -0300

    feature: header responsivity

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export default function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export default function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Davys Lima</Text>
-        <Text color="gray.300" fontSize="small">
-          dlima@direto.tech
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Davys Lima</Text>
+          <Text color="gray.300" fontSize="small">
+            dlima@direto.tech
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Davys Lima" />
     </Flex>
