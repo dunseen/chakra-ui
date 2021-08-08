5f0c09f61eabd7fea82a4881b4d20bb838dc2b7a
Davys Lima <davysjunior08@hotmail.com>
2021-08-08 19:55:31 -0300

    feature: refetch login

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -23,7 +23,7 @@ import Sidebar from "../../components/Sidebar";
 import { useQuery } from "react-query";
 
 export default function UsersList() {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, isFetching, error } = useQuery(
     "users",
     async () => {
       const response = await fetch("http://localhost:3000/api/users");
@@ -61,6 +61,7 @@ export default function UsersList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && <Spinner size="sm" ml="6" />}
             </Heading>
             <Link href="/users/create" passHref>
               <Button
